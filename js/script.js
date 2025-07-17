6881577c1f88b25fe836b928c82d6a71c7da0262
Marvello Faisal <marvellbrazil@gmail.com>
2025-07-17 17:27:51 +0700

    added alert to JS

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
 function execute() {
-    const nama = document.getElementById('nama').value;
-    const tl = document.getElementById('tgl_lahir').value;
-    const jk = document.getElementById('jk').value;
-    const pesan = document.getElementById('pesan').value;
+  const nama = document.getElementById("nama").value;
+  const tl = document.getElementById("tgl_lahir").value;
+  const jk = document.getElementById("jk").value;
+  const pesan = document.getElementById("pesan").value;
 
-    const time = document.getElementById('currtime');
-    const namaout = document.getElementById('namaout');
-    const tlout = document.getElementById('tlout');
-    const jkout = document.getElementById('jkout');
-    const pesanout = document.getElementById('msgout');
+  const time = document.getElementById("currtime");
+  const namaout = document.getElementById("namaout");
+  const tlout = document.getElementById("tlout");
+  const jkout = document.getElementById("jkout");
+  const pesanout = document.getElementById("msgout");
 
-    const now = new Date();
+  const now = new Date();
 
-    time.innerHTML = now.toString().substring(0, now.toString().indexOf('(') - 1);
+  if (nama && tl && pesan) {
+    time.innerHTML = now
+      .toString()
+      .substring(0, now.toString().indexOf("(") - 1);
     namaout.innerHTML = nama;
     tlout.innerHTML = tl;
     jkout.innerHTML = jk;
     pesanout.innerHTML = pesan;
+  } else {
+    window.alert('Mohon untuk melengkapi semua input pada form!');
+  }
 }
